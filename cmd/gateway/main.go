89cff51ae4782a8aa4933603e5e291c4282eaf02
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2023-06-20 21:29:31 +1000

    docker compose and config support

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,196 +1,64 @@
 package main
 
 import (
-	"encoding/hex"
-	"encoding/json"
 	"flag"
 	"log"
-	"math/rand"
-	"net/http"
-	"net/url"
+	"time"
 
 	"github.com/rusq/osenv/v2"
 	"github.com/rusq/vhoster"
+	"github.com/rusq/vhoster/apiserver"
 )
 
 var (
-	host    = flag.String("gateway-host", osenv.Value("GATEWAY_HOST", "localhost"), "host to listen on")
-	port    = flag.String("gateway-port", osenv.Value("GATEWAY_PORT", "8081"), "port to listen on")
-	pubAddr = flag.String("pub", osenv.Value("PUBLIC_ADDRESS", "localhost:8081"), "server public `hostname`, it is used as a suffix for all vhosts")
+	addr    = flag.String("addr", osenv.Value("GATEWAY_ADDRESS", "localhost:8080"), "gateway address (host:port)")
+	pubAddr = flag.String("domain", osenv.Value("DOMAIN", "localhost:8080"), "server public domain `name`, it is used as a suffix for all vhosts, e.g. vhost1.public-hostname.com.  It must include custom port, if it uses one.")
 	apiaddr = flag.String("api", osenv.Value("API_ADDRESS", "localhost:8083"), "address of this api server that controls the gateway")
+	config  = flag.String("c", osenv.Value("CONFIG", ""), "path to the optional config file in JSON format.")
 )
 
 func main() {
 	flag.Parse()
 
-	s, err := vhoster.Listen(*host + ":" + *port)
+	cfg, hosts, err := parseCmdLine()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := vhoster.Listen(cfg.GatewayAddress, vhoster.WithHosts(hosts), vhoster.WithTimeout(time.Duration(cfg.Timeout)))
 	if err != nil {
 		log.Fatal(err)
 	}
 	go s.Wait()
 
-	gateway := &gateway{
-		addr: *pubAddr,
-		vs:   s,
-	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/vhost/", Only(gateway.handleVhost, http.MethodPost, http.MethodDelete, http.MethodGet))
-	mux.HandleFunc("/random/", Only(gateway.handleRandom, http.MethodPost))
-	mux.HandleFunc("/health/", Only(gateway.handleHealth, http.MethodGet))
-
-	log.Printf("Gateway: listening on %s:%s", *host, *port)
-	log.Printf("API: listening on %s", *apiaddr)
-	log.Fatal(http.ListenAndServe(*apiaddr, mux))
+	log.Fatal(apiserver.Run(s, cfg.APIAddress, cfg.DomainName))
 }
 
-type vhostmgr interface {
-	Add(string, *url.URL) error
-	Remove(string) error
-	List() []vhoster.Host
-}
-
-type gateway struct {
-	addr string
-	vs   vhostmgr
-}
-
-type AddRequest struct {
-	HostPrefix string `json:"host_prefix,omitempty"`
-	Target     string `json:"target,omitempty"`
-}
-
-type AddResponse struct {
-	Hostname string `json:"hostname,omitempty"`
-}
-
-func Only(h http.HandlerFunc, methods ...string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		for _, m := range methods {
-			if r.Method == m {
-				h(w, r)
-				return
-			}
+func parseCmdLine() (*Config, []vhoster.Host, error) {
+	var cfg = Config{}
+	if *config != "" {
+		if err := loadConfig(*config, &cfg); err != nil {
+			log.Fatal(err)
 		}
-		w.WriteHeader(http.StatusBadRequest)
 	}
-}
+	// override config with command line flags.
+	cfg.GatewayAddress = coalesce(*addr, cfg.GatewayAddress)
+	cfg.APIAddress = coalesce(*apiaddr, cfg.APIAddress)
+	cfg.DomainName = coalesce(*pubAddr, cfg.DomainName)
+	if cfg.Timeout == 0 {
+		cfg.Timeout = duration(5 * time.Second)
+	}
 
-func (g *gateway) handleVhost(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		// create
-		g.handleAdd(w, r)
-	case http.MethodDelete:
-		// delete
-		g.handleRemove(w, r)
-	case http.MethodGet:
-		// list
-		g.handleList(w, r)
-	}
-}
-
-func (g *gateway) handleAdd(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var req AddRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		log.Print("error decoding body:", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	g.processAdd(w, r, &req)
-}
-
-func (g *gateway) processAdd(w http.ResponseWriter, r *http.Request, req *AddRequest) {
-	if req.Target == "" {
-		log.Print("empty target")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	uri, err := url.Parse(req.Target)
+	hosts, err := cfg.Hosts()
 	if err != nil {
-		log.Print("error parsing the target hostname:", err)
-		w.WriteHeader(http.StatusNotAcceptable)
-		return
+		log.Fatal(err)
 	}
-
-	vhost := req.HostPrefix + "." + g.addr
-	if _, err := url.Parse(vhost); err != nil {
-		log.Print("error parsing the resulting hostname:", err)
-		w.WriteHeader(http.StatusNotAcceptable)
-		return
-	}
-	if err := g.vs.Add(vhost, uri); err != nil {
-		log.Print("error adding host:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(AddResponse{Hostname: vhost})
+	return &cfg, hosts, nil
 }
 
-type vhost struct {
-	Host string `json:"host,omitempty"`
-	URI  string `json:"uri,omitempty"`
-}
-
-type ListResponse struct {
-	Hosts []vhost
-}
-
-func (g *gateway) handleList(w http.ResponseWriter, r *http.Request) {
-	hosts := g.vs.List()
-	w.Header().Set("Content-Type", "application/json")
-	var resp ListResponse
-	for _, h := range hosts {
-		resp.Hosts = append(resp.Hosts, vhost{Host: h.Name, URI: h.URI.String()})
+func coalesce(a, b string) string {
+	if a != "" {
+		return a
 	}
-	json.NewEncoder(w).Encode(resp)
-}
-
-type RandomResponse struct {
-	AddResponse
-}
-
-type RandomRequest struct {
-	Target string `json:"target,omitempty"`
-}
-
-// handleRandom creates a random hostname and adds it to the gateway
-// it returns the hostname
-func (g *gateway) handleRandom(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-
-	// generate a random uuid without using external libs
-	baVhost := [16]byte{}
-	for i := 0; i < 16; i++ {
-		baVhost[i] = byte(rand.Intn(256))
-	}
-	sVhost := hex.EncodeToString(baVhost[:])
-	var req RandomRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		log.Print("error decoding body:", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	g.processAdd(w, r, &AddRequest{HostPrefix: sVhost, Target: req.Target})
-}
-
-func (g *gateway) handleHealth(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
-}
-
-func (g *gateway) handleRemove(w http.ResponseWriter, r *http.Request) {
-	// remove
-	vhost := r.URL.Path[len("/vhost/"):]
-	if vhost == "" {
-		log.Print("got empty vhost")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if err := g.vs.Remove(vhost + "." + g.addr); err != nil {
-		log.Print("error removing host:", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	return b
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,11 @@ go 1.20
 require (
 	github.com/inconshreveable/go-vhost v1.0.0
 	github.com/rusq/osenv/v2 v2.0.1
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
